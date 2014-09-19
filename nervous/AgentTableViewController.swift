c1ad71938e2599030a089c99cd3dc045299834be
Sam Sulaimanov <sam@bitmorse.com>
2014-09-19 14:22:44 +0200

    Adding sensor creation windows.

---
+++ nervous/AgentTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AgentTableViewController.swift
+//  nervous
+//
+//  Created by Sam Sulaimanov on 11/09/14.
+//  Copyright (c) 2014 ethz. All rights reserved.
+//
+
+import UIKit
+
+class AgentTableViewController: UITableViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+             }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- nervous/ProfileViewController.swift
+++ nervous/ProfileViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class ProfileViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
