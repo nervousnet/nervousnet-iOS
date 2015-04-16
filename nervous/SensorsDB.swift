54295331a1f44924c034d0a74a9b38787bc92c64
sforte <fortesimone90@gmail.com>
2015-04-16 16:22:00 +0200

    Storage engine initial implementation

---
+++ nervous/SensorsDB.swift
@@ -0,0 +1,14 @@
+//
+//  SensorsDB.swift
+//  nervousnet
+//
+//  Created by Simone on 09/04/15.
+//  Copyright (c) 2015 ethz. All rights reserved.
+//
+
+import Foundation
+
+protocol SensorsDB {    
+    func retrieve(sensorId: UInt64, fromTimestamp: UInt64, toTimestamp: UInt64) -> [Message]
+    func store(sensorId: UInt64, timestamp: UInt64, sensorData: Message)
+}
