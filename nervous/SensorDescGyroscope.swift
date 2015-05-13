7cd3b068d144e3b4500821031badf859d87de65c
Sid <sid027@gmail.com>
2015-05-13 17:18:03 +0200

    New Full Commit

--- nervous/SensorDescGyroscope.swift
+++ nervous/SensorDescGyroscope.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import CoreLocation
+
+class SensorDescgyroscope : SensorDesc {
+    
+    let SENSOR_ID :Int64 = 0x000000000000000B
+    
+    var timestamp :UInt64
+    var X :Int32
+    var Y :Int32
+    var Z :Int32
+    
+    init(timestamp :UInt64, X :Int32, Y :Int32, Z :Int32){
+        
+        
+        self.X = X
+        self.Y = Z
+        self.Z = Z
+        self.timestamp = timestamp
+    }
+
+    
+    func toProtoSensor() -> SensorUploadSensorData {
+        
+    }
+    
+}
