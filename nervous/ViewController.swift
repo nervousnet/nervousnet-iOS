f07c2215b93c670e863ac5b7fe56ab693ef0ca42
Sid <sid027@gmail.com>
2015-05-22 16:03:47 +0200

    moved GCD call to AppDelegate

--- nervous/ViewController.swift
+++ nervous/ViewController.swift
@@ -8,12 +8,5 @@ class ViewController : UIViewController {
         println("hi")
         
         // Do any additional setup after loading the view, typically from a nib.
-        dispatch_async(dispatch_get_global_queue(Int(QOS_CLASS_BACKGROUND.value), 0)) { // 1
-            
-            dispatch_async(dispatch_get_main_queue()) { // 2
-                SensorCollection.sensorActivate(CMMotionManager())
-                //println("hello")
-            }
-        }
     }
 }
