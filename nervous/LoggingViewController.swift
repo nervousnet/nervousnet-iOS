8f86c3dbb7fc8a373f7881307711635bce49437f
Lewin Koenemann <lewinkoenemann@gmail.com>
2015-05-24 16:16:50 +0200

    rudimentary functionality for logging interval controls

--- nervous/LoggingViewController.swift
+++ nervous/LoggingViewController.swift
@@ -7,15 +7,15 @@ class LoggingViewController : UIViewController {
     @IBOutlet weak var accField: UITextField!
     @IBOutlet weak var batField: UITextField!
     @IBOutlet weak var bleField: UITextField!
-//    @IBOutlet weak var conField: UITextField!
-//    @IBOutlet weak var gyrField: UITextField!
-//    @IBOutlet weak var humField: UITextField!
-//    @IBOutlet weak var ligField: UITextField!
-//    @IBOutlet weak var magField: UITextField!
-//    @IBOutlet weak var noiField: UITextField!
-//    @IBOutlet weak var preField: UITextField!
-//    @IBOutlet weak var proField: UITextField!
-//    @IBOutlet weak var temField: UITextField!
+    @IBOutlet weak var conField: UITextField!
+    @IBOutlet weak var gyrField: UITextField!
+    @IBOutlet weak var humField: UITextField!
+    @IBOutlet weak var ligField: UITextField!
+    @IBOutlet weak var magField: UITextField!
+    @IBOutlet weak var noiField: UITextField!
+    @IBOutlet weak var preField: UITextField!
+    @IBOutlet weak var proField: UITextField!
+    @IBOutlet weak var temField: UITextField!
     
     var values = [0,1,2,3,4]
     
@@ -24,9 +24,9 @@ class LoggingViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         println("hilo")
-        accField.text = String(values[0])
-        batField.text = String(values[0])
-        bleField.text = String(values[0])
+//        accField.text = String(values[0])
+//        batField.text = String(values[0])
+//        bleField.text = String(values[0])
 //        conField.text = String(values[0])
 //        gyrField.text = String(values[0])
 //        humField.text = String(values[0])
@@ -53,41 +53,42 @@ class LoggingViewController : UIViewController {
         bleField.text = String(values[abs((Int(sender.value))%(values.count))])
     }
     
-//    @IBAction func conChange(sender: UIStepper) {
-//        conField.text = String(values[abs((Int(sender.value))%(values.count))])
-//    }
-//    
-//    @IBAction func gyrChange(sender: UIStepper) {
-//        gyrField.text = String(values[abs((Int(sender.value))%(values.count))])
-//    }
-//    
-//    @IBAction func humChange(sender: UIStepper) {
-//        humField.text = String(values[abs((Int(sender.value))%(values.count))])
-//    }
-//    
-//    @IBAction func ligChange(sender: UIStepper) {
-//        ligField.text = String(values[abs((Int(sender.value))%(values.count))])
-//    }
-//    
-//    @IBAction func magChange(sender: UIStepper) {
-//        magField.text = String(values[abs((Int(sender.value))%(values.count))])
-//    }
-//    
-//    @IBAction func noiChange(sender: UIStepper) {
-//        noiField.text = String(values[abs((Int(sender.value))%(values.count))])
-//    }
-//    
-//    @IBAction func preChange(sender: UIStepper) {
-//        preField.text = String(values[abs((Int(sender.value))%(values.count))])
-//    }
-//    
-//    @IBAction func proChange(sender: UIStepper) {
-//        proField.text = String(values[abs((Int(sender.value))%(values.count))])
-//    }
-//    
-//    @IBAction func temChange(sender: UIStepper) {
-//        temField.text = String(values[abs((Int(sender.value))%(values.count))])
-//    }
+    @IBAction func conChange(sender: UIStepper) {
+        conField.text = String(values[abs((Int(sender.value))%(values.count))])
+    }
+
+    @IBAction func gyrChange(sender: UIStepper) {
+        gyrField.text = String(values[abs((Int(sender.value))%(values.count))])
+    }
+
+    @IBAction func humChange(sender: UIStepper) {
+        humField.text = String(values[abs((Int(sender.value))%(values.count))])
+    }
+    
+    @IBAction func ligChange(sender: UIStepper) {
+        ligField.text = String(values[abs((Int(sender.value))%(values.count))])
+    }
+
+    @IBAction func magChange(sender: UIStepper) {
+        magField.text = String(values[abs((Int(sender.value))%(values.count))])
+    }
+    
+    @IBAction func noiChange(sender: UIStepper) {
+        noiField.text = String(values[abs((Int(sender.value))%(values.count))])
+    }
+
+    @IBAction func preChange(sender: UIStepper) {
+        preField.text = String(values[abs((Int(sender.value))%(values.count))])
+    }
+
+    @IBAction func proChange(sender: UIStepper) {
+        proField.text = String(values[abs((Int(sender.value))%(values.count))])
+    }
+    
+    @IBAction func temChange(sender: UIStepper) {
+        temField.text = String(values[abs((Int(sender.value))%(values.count))])
+
+    }
     
     
     
