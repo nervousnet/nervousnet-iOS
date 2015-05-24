6701095df2ae025d23505461dc64c66b150c0a9c
Lewin Koenemann <lewinkoenemann@gmail.com>
2015-05-24 14:36:04 +0200

    Scroll views now working as intended with only vertical scroll + bounce

---
+++ nervous/LoggingViewController.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+import CoreMotion
+
+class LoggingViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("hilo")
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
---
+++ nervous/PrivacyViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+import CoreMotion
+
+class PrivacyViewController : UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       // scrollView.contentSize = CGSize(width: 100, height : 3000)
+        println("hi")
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
