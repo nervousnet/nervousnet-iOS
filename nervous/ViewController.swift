fc03b885fc247015e8da9fe3d35e851a04d51caf
Lewin Koenemann <lewinkoenemann@gmail.com>
2015-05-26 14:56:45 +0200

    added nervous apps screen

--- nervous/ViewController.swift
+++ nervous/ViewController.swift
@@ -3,9 +3,11 @@ import CoreMotion
 
 class ViewController : UIViewController {
     
+    @IBOutlet var Background: UIImageView!
+    @IBOutlet var button: UIButton!
     override func viewDidLoad() {
-        super.viewDidLoad()
         
+        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 }
