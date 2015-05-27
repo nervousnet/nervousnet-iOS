32174d63f41b73219ec42f23d62f49e0f44c22ca
Lewin Koenemann <lewinkoenemann@gmail.com>
2015-05-27 17:37:49 +0200

    UI functionality commit

---
+++ nervous/NervousAppsViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class NervousAppsViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func earthquake(sender: UIButton) {
+        println("\n")
+        println("DAS ISSES")
+    }
+    @IBAction func nervousnetBand(sender: UIButton) {
+        println("\n")
+        println("DAS ISSES")
+    }
+    @IBAction func trafficDeJammer(sender: UIButton) {
+        println("\n")
+        println("DAS ISSES")
+    }
+    
+}
