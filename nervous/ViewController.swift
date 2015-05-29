0f8acc2df63f6f5c6ce3fe439d6bf5ec34c41cb2
Lewin Koenemann <lewinkoenemann@gmail.com>
2015-05-29 16:08:14 +0200

    peanuts

--- nervous/ViewController.swift
+++ nervous/ViewController.swift
@@ -5,9 +5,17 @@ class ViewController : UIViewController {
     
     @IBOutlet var Background: UIImageView!
     @IBOutlet var button: UIButton!
+    @IBOutlet var Killswitch: UISwitch!
+    
     override func viewDidLoad() {
         
+        
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func KillSwitch(sender: UISwitch) {
+        println("\n")
+        print(sender.on)
     }
 }
