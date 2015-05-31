eaba4ecac1cb6868f04221dc2cc922790f1c05e7
Lewin Koenemann <lewinkoenemann@gmail.com>
2015-05-31 15:07:11 +0200

    highcharts demo working

--- nervous/SensorStatisticsViewController.swift
+++ nervous/SensorStatisticsViewController.swift
@@ -17,31 +17,32 @@ class SensorStatisticsViewController : UIViewController {
             "var plot_title = " + "'Proximity data';" +
             "var plot_subtitle = " + "'';"
         print(javascript_global_variables)
-        //let url = NSBundle.mainBundle().URLForResource("webview_charts_1_basic_columns_over_time", withExtension: "html")
         
-        var nsu = NSURL(string: "javascript:" + javascript_global_variables)
-        var req = NSURLRequest(URL: nsu!)
-        webView.loadRequest(req)
+        let url = NSBundle.mainBundle().URLForResource("hello", withExtension: "htm")
         
-        nsu = NSBundle.mainBundle().URLForResource("webview_charts_1_line_live_data_over_time", withExtension: "html")
-        req = NSURLRequest(URL: nsu!)
-        webView.loadRequest(req)
+//        var nsu = NSURL(string: "javascript:" + javascript_global_variables)
+//        var req = NSURLRequest(URL: nsu!)
+//        webView.loadRequest(req)
+//        
+//        nsu = NSBundle.mainBundle().URLForResource("webview_charts_1_line_live_data_over_time", withExtension: "html")
+//        req = NSURLRequest(URL: nsu!)
+//        webView.loadRequest(req)
+//        
+//        
+//        
+//        
+//        nsu = NSURL(string: "javascript:" + "point = [Date.UTC(2015,11,23,11,32,52),83];")
+//        println("\n" + nsu!.description)
+////            (string: "javascript:" + "point = " + "[Date.UTC("
+////            + 2015.description + "," + 11.description + "," + 23.description + "," + 11.description
+////            + "," + 32.description + "," + 52.description + "),"
+////            + 83.description + "];")
+//        req = NSURLRequest(URL: nsu!)
+//        webView.loadRequest(req)
         
-        
-        
-        
-        nsu = NSURL(string: "javascript:" + "point = [Date.UTC(2015,11,23,11,32,52),83];")
-        println("\n" + nsu!.description)
-//            (string: "javascript:" + "point = " + "[Date.UTC("
-//            + 2015.description + "," + 11.description + "," + 23.description + "," + 11.description
-//            + "," + 32.description + "," + 52.description + "),"
-//            + 83.description + "];")
-        req = NSURLRequest(URL: nsu!)
-        webView.loadRequest(req)
-        
-//        let requestURL = NSURL(string: "http://www.google.com")
-//        let request = NSURLRequest(URL : url!)
-//        webView.loadRequest(request)
+        let requestURL = NSURL(string: "http://www.google.com")
+        let request = NSURLRequest(URL : url!)
+        webView.loadRequest(request)
     }
 
 }
