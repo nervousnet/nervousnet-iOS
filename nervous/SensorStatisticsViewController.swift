6d26581ffbb6b8f2353e9d894fb0b8c4b73f0768
Lewin Koenemann <lewinkoenemann@gmail.com>
2015-05-31 15:16:58 +0200

    IT FINALLY GRAPHS THE POINT

--- nervous/SensorStatisticsViewController.swift
+++ nervous/SensorStatisticsViewController.swift
@@ -20,29 +20,25 @@ class SensorStatisticsViewController : UIViewController {
         
         let url = NSBundle.mainBundle().URLForResource("hello", withExtension: "htm")
         
-//        var nsu = NSURL(string: "javascript:" + javascript_global_variables)
-//        var req = NSURLRequest(URL: nsu!)
-//        webView.loadRequest(req)
-//        
-//        nsu = NSBundle.mainBundle().URLForResource("webview_charts_1_line_live_data_over_time", withExtension: "html")
-//        req = NSURLRequest(URL: nsu!)
-//        webView.loadRequest(req)
-//        
-//        
-//        
-//        
-//        nsu = NSURL(string: "javascript:" + "point = [Date.UTC(2015,11,23,11,32,52),83];")
-//        println("\n" + nsu!.description)
-////            (string: "javascript:" + "point = " + "[Date.UTC("
-////            + 2015.description + "," + 11.description + "," + 23.description + "," + 11.description
-////            + "," + 32.description + "," + 52.description + "),"
-////            + 83.description + "];")
-//        req = NSURLRequest(URL: nsu!)
-//        webView.loadRequest(req)
+        webView.stringByEvaluatingJavaScriptFromString("javascript:" + javascript_global_variables)
         
-        let requestURL = NSURL(string: "http://www.google.com")
-        let request = NSURLRequest(URL : url!)
-        webView.loadRequest(request)
+        var nsu = NSBundle.mainBundle().URLForResource("webview_charts_1_line_live_data_over_time", withExtension: "html")
+        var req = NSURLRequest(URL: nsu!)
+        webView.loadRequest(req)
+        
+        
+        
+        
+        webView.stringByEvaluatingJavaScriptFromString("javascript:" + "point = [Date.UTC(2015,11,23,11,32,52),83];")
+        
+//            (string: "javascript:" + "point = " + "[Date.UTC("
+//            + 2015.description + "," + 11.description + "," + 23.description + "," + 11.description
+//            + "," + 32.description + "," + 52.description + "),"
+//            + 83.description + "];")
+        
+//        let requestURL = NSURL(string: "http://www.google.com")
+//        let request = NSURLRequest(URL : url!)
+//        webView.loadRequest(request)
     }
 
 }
