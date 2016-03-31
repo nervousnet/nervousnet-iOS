aa147684062537afdc629f9fd48a56994e351750
Sid <sid027@gmail.com>
2016-03-31 16:12:07 +0200

    LAEController change to HubController

---
+++ nervous/Controllers/QueryController.swift
@@ -0,0 +1,16 @@
+//
+//  QueryController.swift
+//  nervousnet
+//
+//  Created by spadmin on 31/03/16.
+//  Copyright © 2016 ethz. All rights reserved.
+//
+
+import Foundation
+
+// this is the LAEController
+// LAEController.swift file should be deleted
+class QueryController : NSObject {
+    
+    
+}
---
+++ nervous/Controllers/RegisterController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterController.swift
+//  nervousnet
+//
+//  Created by spadmin on 31/03/16.
+//  Copyright © 2016 ethz. All rights reserved.
+//
+
+import Foundation
+
+// this is used to add virtual sensors that developers
+// want to add to the nervousne system
+class RegisterController : NSObject {
+    
+}
