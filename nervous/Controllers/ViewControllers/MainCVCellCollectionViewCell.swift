dc9c29f3b2fca9875b658ca9ba9f1277623842a3
Sam Sulaimanov <sam@bitmorse.com>
2016-04-03 21:08:10 +0200

    adding springboard

---
+++ nervous/Controllers/ViewControllers/MainCVCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MainCVCellCollectionViewCell.swift
+//  nervousnet
+//
+//  Created by Sam Sulaimanov on 03/04/16.
+//  Copyright © 2016 ethz. All rights reserved.
+//
+
+import UIKit
+
+class MainCVCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    
+}
--- nervous/Controllers/ViewControllers/MainCollectionViewController.swift
+++ nervous/Controllers/ViewControllers/MainCollectionViewController.swift
@@ -11,7 +11,34 @@ import UIKit
 
 class MainCollectionViewController: UICollectionViewController  {
     
-   
+    private let reuseIdentifier = "MainCVCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
     
-
+    func getNumberOfCellsDisplayable() -> Int {
+        return 30;
+    }
 }
+
+extension MainCollectionViewController {
+    
+    //1
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    //2
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return getNumberOfCellsDisplayable();
+    }
+    
+    //3
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        //1
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! MainCVCellCollectionViewCell
+        //2
+        cell.backgroundColor = UIColor.orangeColor()
+        //3
+        cell.imageView.image = UIImage(imageLiteral: "3rd-floor-0")
+        
+        return cell
+    }}
