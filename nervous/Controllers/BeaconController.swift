18c60004640b72edcf73fc8265d43592d67a9c58
Danny Thuering <dthuering@gmail.com>
2016-04-22 20:18:54 +0200

    make beacon controller a singleton too

--- nervous/Controllers/BeaconController.swift
+++ nervous/Controllers/BeaconController.swift
@@ -15,40 +15,43 @@ protocol BeaconControllerDelegate {
 }
 
 
+let _sharedInstance = BeaconController()
+
 class BeaconController : NSObject, SensorProtocol{
     
     var delegate: BeaconControllerDelegate?
     let locationManager : CLLocationManager
     let beaconRegion : CLBeaconRegion
-    let beaconUUIDString = NSUUID(UUIDString: "3C77C2A5-5D39-420F-97FD-E7735CC7F317")!
+//		let beaconUUIDString = NSUUID(UUIDString: "3C77C2A5-5D39-420F-97FD-E7735CC7F317")!
+		let beaconUUIDString = NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!
     let beaconIdentifier = "ch.ethz.nervous"
     let beaconMajor:CLBeaconMajorValue = 33091
-    
-    
+	
+		var beaconData = [AnyObject]()
+	
     override init() {
- 
         self.locationManager = CLLocationManager()
         self.beaconRegion = CLBeaconRegion(proximityUUID: beaconUUIDString, identifier: beaconIdentifier)
     }
     
-    
+		class var sharedInstance: BeaconController {
+			return _sharedInstance
+		}
+	
     func requestAuthorization() {
         locationManager.requestAlwaysAuthorization()
     }
-    
     
     func startSensorUpdates() {
         locationManager.delegate = self
         locationManager.startMonitoringForRegion(beaconRegion)
         locationManager.startRangingBeaconsInRegion(beaconRegion)
     }
-    
-    
+	
     func stopSensorUpdates() {
         locationManager.stopMonitoringForRegion(beaconRegion)
         locationManager.stopRangingBeaconsInRegion(beaconRegion)
     }
-    
 }
 
 extension BeaconController: CLLocationManagerDelegate {
@@ -62,7 +65,11 @@ extension BeaconController: CLLocationManagerDelegate {
     }
     
     func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
-        
+			
+			self.beaconData = beacons.map({ (beacon) -> [String:String] in
+					return ["uuid": beacon.proximityUUID.UUIDString, "major": String(beacon.major), "minor": String(beacon.minor), "proximity": String(beacon.proximity.rawValue), "accuracy": String(format:"%f", beacon.accuracy)]
+				})
+			
         //send new beacons to delegates
         if let delegate = self.delegate {
             delegate.controller(self, didRangeBeacons: beacons)
